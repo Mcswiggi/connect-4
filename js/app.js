fe8003de588c01402976578bcb65d37b7d5eec71
Mcswiggi <mrchitrason@gmail.com>
2021-07-02 16:21:47 -0400

    sclarified psudocode, started html grid and css

--- js/app.js
+++ js/app.js
@@ -1,20 +1,42 @@
 //connect 4 psudeocode
-// 1) define variables used to track the state of the game (playerTurn, gameBoard, isWinner, etc ...)
+// 1) define variables used to track the state of the game (playerTurn, gameBoard, isWinner, etc ...), . 
+    // figure out how to make the gameboard
 
 // 2) store cached element references to connect javascript constants to elements on HTML page (gameBoard , gameMessage, whoseTurn)
 
-// 3) create an initializing function to clear the board and neccisary state variables
+// 3) create an initializing function to clear the board and necessary state variables
 
-// 4) create function render to apply changes to where in the gameBoard array the current player is going to drop their game piece.  Within this function i should have some logic that the game piece will go to the bottom of the column depending on if there is a game piece there or not. display a message for whose turn it is. prompt for player names
+// create start button and clicked (init), prompt for player names and board appears
 
-// 5) create a checkWinner function to check for winning combinations of 4.  I want to include an easteregg to check for 5 in a row
+// create a function that determines who goes first
+
+// create timer function for after player names are entered is pressed
+
+// 4) create function render to apply changes to where in the gameBoard array the current player is going to drop their game piece.  Within this function i should have some logic that the game piece will go to the bottom of the column depending on if there is a game piece there or not.
+// if statmente for if isWinner = false then it is player 1 or 2 turn, if all the spots on the board are not null then it is a 'T' or announce the winner
+// display a message for whose turn it is. prompt for player names
+// display game state. whose turn it is, win, tie
+
+// 5)  after each click, a checkWinner function to check for winning combinations of 4.  I want to include an easteregg to check for 5 in a row
+// also start timer for the next person . -Winning cominations
 
 // 6) define constants. create draw function (gameBoard = !null) is a Tie
-
-// 7) handle clicking a spot on the gameBoard. what color game piece to drop (turn)?  also add a timer to when each player is able to click to add more of a challenge for users
 
 // 8) create a reset button that re-initializes the game state / board / variables
 
 //wireframe: https://wireframe.cc/qUCucN
 
-or create an indexing piece dropper?
+// SUPER BONUS: 
+// keep track of who's the winner
+
+// or create an indexing piece dropper?
+
+/*------Constants------*/
+
+/*------Variables (state)------*/
+
+/*------Cached Element References------*/
+
+/*------Event Listeners------*/
+
+/*------Functions------*/
