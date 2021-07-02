45f57ad329809a87867057475471740248ddbc2d
Mcswiggi <mrchitrason@gmail.com>
2021-07-02 00:10:50 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//connect 4 psudeocode
+// 1) define variables used to track the state of the game (playerTurn, gameBoard, isWinner, etc ...)
+
+// 2) store cached element references to connect javascript constants to elements on HTML page (gameBoard , gameMessage, whoseTurn)
+
+// 3) create an initializing function to clear the board and neccisary state variables
+
+// 4) create function render to apply changes to where in the gameBoard array the current player is going to drop their game piece.  Within this function i should have some logic that the game piece will go to the bottom of the column depending on if there is a game piece there or not. display a message for whose turn it is. prompt for player names
+
+// 5) create a checkWinner function to check for winning combinations of 4.  I want to include an easteregg to check for 5 in a row
+
+// 6) define constants. create draw function (gameBoard = !null) is a Tie
+
+// 7) handle clicking a spot on the gameBoard. what color game piece to drop (turn)?  also add a timer to when each player is able to click to add more of a challenge for users
+
+// 8) create a reset button that re-initializes the game state / board / variables
+
+//wireframe: https://wireframe.cc/qUCucN
+
+or create an indexing piece dropper?
