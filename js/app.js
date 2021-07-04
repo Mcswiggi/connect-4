f25f37e7c5c9ea8e1d9ad1b0a2525cab44bc4f29
Mcswiggi <mrchitrason@gmail.com>
2021-07-04 03:07:45 -0400

    still tinkering with render function, playerTurn and wincondtions

--- js/app.js
+++ js/app.js
@@ -38,16 +38,17 @@ const winningCombinations = []
 const player1 = 1   //prompt("What is player one's name?")
 const player2 = -1  //prompt("What is player two's name?")
 /*------Variables (state)------*/
-let gameBoard, isWinner, playerTurn
+let gameBoard, isWinner, playerTurn, x, y
 /*------Cached Element References------*/
-const gameBoardEl = document.querySelectorAll('div')
+const rowEl = document.getElementsByTagName('tr') //y-direction
+const columnEl = document.getElementsByTagName('td') //x-direction
 const resetBtn = document.querySelector('#restart')
 const messageEl = document.querySelector('#gameMessage')
-
+const cells = document.querySelectorAll('cell')
 /*------Event Listeners------*/
-gameBoardEl.forEach((spot) => {
-    spot.addEventListener('click', handleClick)
-})
+for(i = 0 ; i < columnEl.length ; i++){
+columnEl[i].addEventListener('click', handleClick)
+}
 
 /*------Functions------*/
 
@@ -62,20 +63,19 @@ function init() {
         // [1,2,3,4,5,6],
         // [1,2,3,4,5,6],
         // [1,2,3,4,5,6],
-        [1, null, null, null, null, null],
         [null, null, null, null, null, null],
         [null, null, null, null, null, null],
-        [null, null, null, 1, null, null],
         [null, null, null, null, null, null],
         [null, null, null, null, null, null],
-        [null, null, null, null, null, 1],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
+        [null, null, null, null, null, null],
     ]
-
 
     isWinner = false
     playerTurn = randomTurn() //randomly picks who goes first
     
-    render()
+    //render()
  
 }
 
@@ -89,58 +89,151 @@ function randomTurn () {
 }
 
 function clearText () {
-
+    
 }
 
-function render () {
-    // gameBoard.forEach(function(column, idx) {
-    //     gameBoardEl[idx].style.background= 'blue'
-    //     console.log(gameBoard[idx])
-    //     gameBoard[idx].forEach(function(spot,idx) {
-    //         console.log(spot, idx)
-    //     })
-    //     })
-        // column.forEach(function(spot, idx) {
-        //   spot[idx].style.background = 'blue'  
-        // })
+function handleClick(event) {
+    console.log('clicked')
+    x = event.target.cellIndex
+    y = event.target.parentElement.rowIndex
+  console.log(`${y}, ${x}`)
+    validMove()
+    //isWinner = checkWinner()
+    //render()
+    return 
+}
+//change to gameBoard?
+//iterate through all the cells 
+Array.prototype.forEach.call(columnEl , function (cell) {
+    cell.addEventListener('click' , render)
+    cell.style.backgroundColor = 'white'
+})
 
-    // })
+function render (event) {
+    // retrieving what column we are working in
+    let column = event.target.cellIndex
+    // row starts at 5(bottom) and incraments to 0 as the top index
+    // makes sure the game piece will go to the next available spot
+    let row = []
 
-    
-    if (isWinner === false) {
-        if(playerTurn === 1){
-            messageEl.innerHTML = `It is ${player1}'s turn!`
-        } else {
-            messageEl.innerHTML = `It is ${player2}'s turn!`
-        }
-    } else if (isWinner === 'T') {
-        messageEl.innerHTML = 'It is a tie!'
-    } else {
-        if (isWinner = 1) {
-            messageEl.innerHTML = `Congrats ${player1}! You Win!`
-        } else {
-            messageEl.innerHTML = `Congrats ${player2}! You Win!`
+
+    //handleClick
+    //start from the bottom
+    for (i = 5; i > -1; i--){
+        //.children will let me go into each spot or cell within each row index left and right
+        if (rowEl[i].children[column].style.backgroundColor == 'white'){
+            row.push(rowEl[i].children[column]);
+            if (playerTurn === 1){
+                row[0].style.backgroundColor = 'red';
+                if (horizontalWinCondition() || verticalWinCondition() || diagonalWinCondition() || diagonalWinCondition2()){
+                    messageEl.textContent = `${player1} WINS!!`;
+                    messageEl.style.color = 'red';
+                    return alert(`${player1} WINS!!`);
+                }else if (isWinner === checkTie()){  //-------------------------work on tie games! also line 144
+                    messageEl.textContent = 'DRAW!';
+                    return alert('DRAW!');
+                }else{
+                    messageEl.textContent = `${player2}'s turn`
+                    return playerTurn = -1;
+                }
+            }else{
+                row[0].style.backgroundColor = 'blue';
+                if (horizontalWinCondition() || verticalWinCondition() || diagonalWinCondition() || diagonalWinCondition2()){
+                    messageEl.textContent = `${player2} WINS!!`;
+                    messageEl.style.color = 'blue';
+                    return alert(`${player2} WINS!!`);
+                }else if (isWinner === checkTie()){
+                    messageEl.textContent = 'DRAW!';
+                    return alert('DRAW!');
+                }else{
+                    messageEl.textContent = `${player1}'s turn`;
+                    return playerTurn = 1;
+                }
+                
+            }
         }
     }
+//     for(i = 0; i < columnEl.length; i++) {
+//         for(j = 0; j < rowEl.length; j++){
+//         if (element === 1) {
+//             columnEl.style.background = 'blue'
+//         } else if (element === -1) {
+//             columnEl.style.background = 'red'
+//         } else {
+//             columnEl.style.background = 'white'
+//         }
+//     }
+// }
+
+
+
+// if (isWinner === false) {
+//     if(playerTurn === 1){
+//         messageEl.innerHTML = `It is ${player1}'s turn!`
+//     } else {
+//         messageEl.innerHTML = `It is ${player2}'s turn!`
+//     }
+// } else if (isWinner === 'T') {
+//     messageEl.innerHTML = 'It is a tie!'
+// } else {
+//     if (isWinner = 1) {
+//         messageEl.innerHTML = `Congrats ${player1}! You Win!`
+//     } else {
+//         messageEl.innerHTML = `Congrats ${player2}! You Win!`
+//     }
+// }
 }
 
-function checkWinner() {
+    function checkColor(one , two, three, four) {
+       if (one === two && one === three && one === four)
+       return true
+        // if(gameBoard.includes(null)) {
+            //     return false
+            // } else {
+                // return "T"
+                // }
+    }
+        
+    function horizontalWinCondition() {
+        //checks for horizonal win
+        //iterating through all 6 rows
+        for (let row = 0; row < rowEl.length; i++){
+            for (let col = 0; col < 4; col++){
+                //checking if 4 colors match by retreiving the children nodes of the rows
+                if (checkColor(rowEl[row].children[col].style.backgroundColor, rowEl[row].children[col+1].style.backgroundColor, rowEl[row].children[col+2].style.backgroundColor, rowEl[row].children[col+3].style.backgroundColor)){
+                    return true
+                }
+            }
+        }
 
-}
+    }
 
-function validMove() {
+    function verticalWinCondition() {
 
-}
+    }
 
-function timer() {
+    function diagonalWinCondition() {
 
-}
+    }
 
-function handleClick(spot) {
-    console.log('clicked')
-    console.log(spot.cellIndex)
-    playerTurn *= -1
-    validMove()
-    isWinner = checkWinner()
-    render()
-}
+    function diagonalWinCondition2() {
+
+    }
+
+    function checkTie() {
+    if(gameBoard.includes(null)) {
+        return false
+    } else {
+    return "T"
+    }
+    }
+
+    function validMove() {
+        // if(gameBoard[sqNum] || isWinner === true ) {
+            //     return
+            //     }   
+     }
+        
+     function timer() {
+            
+    }
